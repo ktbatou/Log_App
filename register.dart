41305b34548bdb1ccf29bdd6ea1dd3bb5c30558f
ktbatou <tbatoukaoutar@gmail.com>
2022-02-03 14:47:07 +0100

    fixing the form's fields conterollers and adding social medial auth buttons
    fixing the form's fields, password invisibility, socialMedia auth
    
    t

--- register.dart
+++ register.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:login_app/Socialmedia.dart';
 import 'package:login_app/form.dart';
 
 class Register extends StatefulWidget {
@@ -13,21 +14,43 @@ class _RegisterState extends State<Register> {
     double contextWidth = MediaQuery.of(context).size.width;
     double contextHeight = MediaQuery.of(context).size.height;
     return Scaffold(
+        resizeToAvoidBottomInset: false,
         backgroundColor: Colors.white,
         body: Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                  padding: EdgeInsets.only(top: contextHeight * 0.04),
-                  child: Text('Sign Up',
-                      style: GoogleFonts.poppins(
-                        fontSize: 36,
-                        color: Color(0xff6D678E),
-                      ))),
-              RegisterForm()
-            ],
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                    padding: EdgeInsets.only(top: contextHeight * 0.04),
+                    child: Text('Sign Up',
+                        style: GoogleFonts.poppins(
+                          fontSize: 36,
+                          color: const Color(0xff6D678E),
+                        ))),
+                RegisterForm(),
+                const SizedBox(
+                  height: 20,
+                ),
+                Text("Sign Up With",
+                    style: GoogleFonts.poppins(
+                        fontSize: 12, color: const Color(0xffB2B2B2))),
+                const SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  padding: EdgeInsets.only(left: 10, right: 10),
+                  child: SocialMedia(),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                Text("Already have an account?",
+                    style: GoogleFonts.poppins(
+                        fontSize: 12, color: const Color(0xffB2B2B2))),
+              ],
+            ),
           ),
         ));
   }
